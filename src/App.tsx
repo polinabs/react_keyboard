6b5900807ed309cc344bae1b5c27c54a5729c7d5
Polina <polinabondar4@gmail.com>
2024-12-09 20:10:58 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+interface State {
+  key: string;
+}
+
+export class App extends React.Component<void, State> {
+  state = {
+    key: '',
+  };
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ key: event.key });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  render() {
+    const { key } = this.state;
+    const isKeyPressed = key !== '';
+    return (
+      <div className="App">
+        <p className="App__message">
+          {isKeyPressed
+            ? `The last pressed key is [${key}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
+
+// export const App: React.FC = () => {
+//   const [key, setKey] = useState('');
+//   const isKeyPressed = key !== '';
+
+//   useEffect(() => {
+//     const handleKeyUp = (event: KeyboardEvent) => {
+//       setKey(event.key);
+//     };
+
+//     document.addEventListener('keyup', handleKeyUp);
+
+//     return () => {
+//       document.removeEventListener('keyup', handleKeyUp);
+//     };
+//   }, []);
+
+//   return (
+// <div className="App">
+//   <p className="App__message">
+//     {isKeyPressed
+//       ? `The last pressed key is [${key}]`
+//       : 'Nothing was pressed yet'}
+//   </p>
+// </div>
+//   );
+// };
